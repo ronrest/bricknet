b623e8ba28e444f2ab5440f23ece993e697833ef
ronrest <ronny.coding@gmail.com>
2015-11-23 23:28:01 +1100

    Add Layer class

--- bricknet/__init__.py
+++ bricknet/__init__.py
@@ -18,7 +18,7 @@ class Neural_Network(object):
     You can specify each layer in the neural network to be a different Layer
     object, with potentially very different activation  functions, and
     different ways of propagating forward and backwards.
-    
+
     """
     def __init__(self, layers=[2,3,1]):
         """
@@ -33,6 +33,50 @@ class Neural_Network(object):
         self.numlayers = len(self.layerSizes)
 
         # Initialised Weights as random values between 0 and 1
+        # TODO: add Layer objects instead. 
         self.Weights = [np.random.rand(self.layerSizes[i], \
                                        self.layerSizes[i+1]) \
                         for i in range(self.numlayers - 1)]
+
+
+
+################################################################################
+#                                                                    LAYER CLASS
+################################################################################
+class Layer(object):
+    """
+    A template for layer objects
+
+    Note you will need to implement your own methods.
+    """
+    def __init__(self, size=3, next_size=3, weights=None):
+        """
+
+        :param size: {int} (default = 3)
+
+            Size of this layer
+
+        :param next_size: {int} (default = 3)
+
+            Size of the next layer
+
+        :param weights:  {matrix} (default = 3)
+
+            You can use pre-baked weights if you like.
+
+        :return:
+        """
+        # Layer Dinensions
+        self.size = size
+        self.next_size = next_size
+
+        # Initialise Activations to zeroes
+        self.activated_vals = np.zeros(size)
+
+        # Initialise Weights to random values between 0 and 1, unless prebaked
+        # values have been entered as an argument.
+        if weights == None:
+            self.weights = np.random.rand(next_size, size)
+        else:
+            self.weights = weights
+
