5490f06454191dd5d01a5705731fdc177ba8d39a
ronrest <ronny.coding@gmail.com>
2015-11-24 23:00:01 +1100

    Add Softmax Classifier Layer Class.

---
+++ bricknet/layers/ClassifierSoftmax.py
@@ -0,0 +1,71 @@
+"""====================================================
+    DESCRIPTION
+
+=======================================================
+"""
+__author__ = 'ronny'
+
+import numpy as np
+from bricknet import activations
+from bricknet.layers import Layer
+
+
+class ClassifierSoftmax(Layer):
+    def __init__(self, in_size=3, out_size=3, weights=None):
+        """
+
+        :param in_size:
+        :param out_size:
+        :param weights:
+        :return:
+        """
+        Layer.__init__(self, in_size, out_size, weights)
+
+    def activate(selg, agg):
+        """
+        Sigmoid Activation function
+
+        :param selg:
+        :param agg:
+        :return:
+        """
+        return activations.softmax(agg)
+
+    def back(self, out_errors, input_vals, y, update_weights=True):
+        """
+        Back propagate the errors
+
+        :param out_errors: {arrray like}
+
+            error gradients at the post activation of the layer.
+        :param input_vals:
+        :param y:{array-like object of Boolean elements}
+            An array of booleans, representing a one hot vector of the correct
+            class y.
+        :param update_weights:
+        :return:
+        """
+
+        # Gradient of errors WRT the preactivation of the layer.
+        # z = self.aggregate(input_vals)
+        z = self.preactivated_vals
+
+        #
+
+        # Gradient of Softmax for the correct class WRT preactivations
+        h_y_is_c = self.activated_vals[y]
+        hc = self.acivated_vals
+        hy_only = np.ones(len(hc)) * h_y_is_c
+        errors_preactivation = h_y_is_c - hy_only * hc
+
+
+        errors_input = self.weights.transpose().dot(errors_preactivation)
+        errors_weights = np.outer(errors_preactivation, input_vals)
+
+        if update_weights:
+            self.weights = self.weights + errors_input
+
+        return errors_input
+
+
+
