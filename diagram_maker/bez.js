a0f4e551ebf65241018f9e5423b0e7394d4628c1
ronrest <ronny.coding@gmail.com>
2015-11-28 16:29:47 +1100

    CANVAS JS - Add alpha option to the bezier connection

--- diagram_maker/bez.js
+++ diagram_maker/bez.js
@@ -4,7 +4,7 @@
 // d = How far the control point should stick out relative to the starting
 //     position.
 // d2 = as per d, but relative to the end point.
-function bez(context, x, y, x2, y2, size=3, color="#333333", d=50, d2=50){
+function bez(context, x, y, x2, y2, size=3, color="#AAAAAA", d=50, d2=50, alpha=0.3){
       // -----------------------------------------------------------------------
       // Set the Control Points for the Bezier Curves
       // -----------------------------------------------------------------------
@@ -25,8 +25,11 @@ function bez(context, x, y, x2, y2, size=3, color="#333333", d=50, d2=50){
       // Set Aesthetics and Draw on page
       // -----------------------------------------------------------------------
       // line color
+      var BU_Alpha = context.globalAlpha; // backs up the current alpha setting
+      context.globalAlpha = alpha;        // Change the alpha
       context.strokeStyle = color;
       context.stroke();
+      context.globalAlpha = BU_Alpha;     // Restores the original alpha setting
       }
 
 
