631ab287e6b53d1362878acd6c450f1cb43b50b3
ronrest <ronny.coding@gmail.com>
2015-11-28 15:06:18 +1100

    CANVAS JS - Prettify the code for bez()

--- diagram_maker/bez.js
+++ diagram_maker/bez.js
@@ -5,19 +5,28 @@
 //     position.
 // d2 = as per d, but relative to the end point.
 function bez(context, x, y, x2, y2, size=3, color="#333333", d=50, d2=50){
+      // -----------------------------------------------------------------------
+      // Set the Control Points for the Bezier Curves
+      // -----------------------------------------------------------------------
       var ct1_x = x + d;
       var ct1_y = y;
       var ct2_x = x2 - d2;
       var ct2_y = y2;
 
-
+      // -----------------------------------------------------------------------
+      // Create the Bezier Curves
+      // -----------------------------------------------------------------------
       context.beginPath();
       context.moveTo(x, y);
       context.bezierCurveTo(ct1_x, ct1_y, ct2_x, ct2_y, x2, y2);
       context.lineWidth = size;
 
+      // -----------------------------------------------------------------------
+      // Set Aesthetics and Draw on page
+      // -----------------------------------------------------------------------
       // line color
       context.strokeStyle = color;
       context.stroke();
       }
+
 
