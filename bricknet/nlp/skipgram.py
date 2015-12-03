a049208ef9fd9aa93e7c298dc22a6dc0eb6ca6fb
ronrest <ronny.coding@gmail.com>
2015-12-03 22:34:50 +1100

    skipgram - add probability of word pair function

--- bricknet/nlp/skipgram.py
+++ bricknet/nlp/skipgram.py
@@ -10,3 +10,29 @@ np = pd.np
 #from .. import np
 
 
+# ==============================================================================
+#                                                                 PROB_WORD_PAIR
+# ==============================================================================
+def prob_word_pair(in_word, out_word, in_df, out_df):
+    """
+
+    Using softmax probabilities
+
+    The dataframe of word vectors should be organised as follows:
+        rows: each row represents a single word
+        cols: each column is one of the dimensions of the word vector.
+
+    in_word = string
+    out_word = string
+
+    in_df = dataframe of the input word
+    out_df = dataframe of the output word
+    """
+    in_vec = in_df.loc[in_word]
+    out_vec = out_df[out_word]
+
+    numerator = np.exp(out_vec.dot(in_vec))
+    denominator = (np.exp(out_df.dot(in_vec))).sum()
+
+    return numerator / denominator
+
