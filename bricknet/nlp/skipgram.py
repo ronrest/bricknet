848f48fa7aecc1a6a477987e81c07490a43d2517
ronrest <ronny.coding@gmail.com>
2015-12-03 22:32:33 +1100

    Add a skipgram module

---
+++ bricknet/nlp/skipgram.py
@@ -0,0 +1,12 @@
+################################################################################
+#                                                            WORD 2 VEC SKIPGRAM
+################################################################################
+__author__ = 'ronny'
+
+
+#TODO: maybe have pandas load from root module.
+import pandas as pd
+np = pd.np
+#from .. import np
+
+
