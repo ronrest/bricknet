ef91a5b5f6649d1a02f7cda876e0df2e959bcb15
ronrest <ronny.coding@gmail.com>
2015-12-15 09:43:56 +1100

    CBOW_NGG- fix two issues with pow argumnet in get_sampling_distribution () - renamed pow to power to fix issue of the argumnet clashing with the built in mame "pow" - Fix issue of all values returned being 0 when argument is an integer

--- bricknet/nlp/cbow_ngg.py
+++ bricknet/nlp/cbow_ngg.py
@@ -7,10 +7,13 @@ __author__ = 'ronny'
 
 
 import numpy as np
-def get_sampling_distribution(c, pow=3/4.0):
+# ==============================================================================
+#                                                      GET_SAMPLING_DISTRIBUTION
+# ==============================================================================
+def get_sampling_distribution(c, power=3/4.0):
     # Typecast to a numpy array if it is not already an array or a pandas series
     if not isinstance(c, (np.ndarray, pd.Series)):
         c = np.array(c)
 
-    p = c**pow           # Raise unigram counts to the 3/4
+    p = c**float(power)  # Raise unigram counts to the 3/4
     return  p/p.sum()    # Normalize to make values add up to 1
