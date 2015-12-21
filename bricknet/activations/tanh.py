00a8a0496fd6c2a5fc4dc722d4279635cdc44dd9
ronrest <ronny.coding@gmail.com>
2015-12-21 22:29:38 +1100

    ACTIVATIONS - Add docstring to tanh()

--- bricknet/activations/tanh.py
+++ bricknet/activations/tanh.py
@@ -1,5 +1,19 @@
 __author__ = 'ronny'
 
+
+# ==============================================================================
+#                                                                           TANH
+# ==============================================================================
 def tanh(z):
+    """
+    :param z: {array-like}
+
+        the array representing the pre-activation values vector.
+
+    :return:
+
+        Post activation values using the tanh activation function.
+    """
+    # ==========================================================================
     return (z > 0) * z
 
