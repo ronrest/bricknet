a46ad99a3f5dd69153a54a233db2597b94c5c8db
ronrest <ronny.coding@gmail.com>
2015-12-21 22:31:37 +1100

    ACTIVATIONS - Add docstring to tanh_prime()

--- bricknet/activations/tanh.py
+++ bricknet/activations/tanh.py
@@ -18,5 +18,29 @@ def tanh(z):
     return (z > 0) * z
 
 
+# ==============================================================================
+#                                                                           TANH
+# ==============================================================================
 def tanh_prime(z, as_int=True):
+    """
+    :param z: {numpy array}
+
+        the array representing the pre-activation values vector.
+
+    :param as_int: {boolean}
+
+        returns an array of integers (3.88 times faster than returning array of
+        floats). Since the gradients will always be 0 or 1 for the tanh
+        function, then there is no loss of information.
+
+        default = True
+
+        If False, return array of floats.
+
+    :return:
+
+        Gradient of the tanh activation function evaluated at the values in z.
+    """
+    # ==========================================================================
     return (z > 0).astype(int) if as_int else (z > 0).astype(float)
+
